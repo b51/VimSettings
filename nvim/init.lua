783b082ac54279c2d8bc55b400b86846c3fc2f95
b51 <b51live@gmail.com>
2025-10-30 12:42:56 +0800

    Add c file to templates list

--- nvim/init.lua
+++ nvim/init.lua
@@ -130,7 +130,7 @@ end
 vim.api.nvim_create_augroup("AutoTemplateHeader", { clear = true })
 vim.api.nvim_create_autocmd("BufNewFile", {
   group = "AutoTemplateHeader",
-  pattern = { "*.cc", "*.cpp", "*.h", "*.sh", "*.py" },
+  pattern = { "*.c", "*.cc", "*.cpp", "*.h", "*.hpp", "*.sh", "*.py" },
   callback = insert_header_from_template,
 })
 
